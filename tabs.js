465fcf346492e78449c39bc3267db46d307c8066
jayasetiawan82 <47598046+jayasetiawan82@users.noreply.github.com>
2024-02-16 16:41:35 +1030

    finalize tabs

--- tabs.js
+++ tabs.js
@@ -1,66 +1,51 @@
-const tabList = document.querySelector('[role="tablist"]');
-const tabs = tabList.querySelectorAll('[role="tab"]');
-
-tabList.addEventListener('keydown', changeTabFocus);
-
-tabs.forEach((tab) => {
-    tab.addEventListener('click', changeTabPanel);
-});
-
-
-let tabFocus = 0;
-function changeTabFocus(e) {
-    const keydownLeft = 37;
-    const keydownRight = 39;
-    
-    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-        tabs[tabFocus].setAttribute("tabindex", -1);
-    }
-    
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
-        }
-    }
-    
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-        }
-    }
-    
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
-}
-
-
-
-
+// Function to handle tab changes
 function changeTabPanel(e) {
+
+   // This function handles the tab click event
+
+  // Extract necessary information from the clicked tab
   const targetTab = e.target;
   const targetPanel = targetTab.getAttribute("aria-controls");
   const targetImage = targetTab.getAttribute("data-image");
 
+  // Find the containers for the tabs and main content
   const tabContainer = targetTab.parentNode;
   const mainContainer = tabContainer.parentNode;
 
+  // Hide the previously selected tab and show the new one
   tabContainer
     .querySelector('[aria-selected="true"]')
     .setAttribute("aria-selected", false);
 
   targetTab.setAttribute("aria-selected", true);
 
+  // Hide all panels and images, then show the selected ones
   mainContainer
     .querySelectorAll('[role="tabpanel"]')
     .forEach((panel) => panel.setAttribute("hidden", true));
 
-  mainContainer.querySelector(`#${targetPanel}`).removeAttribute('hidden');
+  mainContainer.querySelector(`#${targetPanel}`).removeAttribute("hidden");
 
   mainContainer
-    .querySelectorAll('picture')
+    .querySelectorAll("picture")
     .forEach((picture) => picture.setAttribute("hidden", true));
 
-  mainContainer.querySelector(`#${targetImage}`).removeAttribute('hidden');
+  mainContainer.querySelector(`#${targetImage}`).removeAttribute("hidden");
 }
+
+// Function to set up tab event listeners for a specific tablist
+function setupTabListeners(tabListId) {
+  // This function sets up click event listeners for tabs within a specific tablist
+
+  // Find the tablist and tabs based on the provided ID
+  const tabList = document.getElementById(tabListId);
+  const tabs = tabList.querySelectorAll('[role="tab"]');
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", changeTabPanel);
+  });
+}
+
+// Set up listeners for both tablists
+setupTabListeners("project-tablist");
+setupTabListeners("playground-tablist");
