128c5e7601f1da881859f2586ee8cb22871987dd
jayasetiawan82 <jayasetiawan28@gmail.com>
2024-02-18 19:07:45 +1030

    stayling shapes

--- scroll.js
+++ scroll.js
@@ -1,14 +1,29 @@
 
 const navBar = document.querySelector(".nav-bar");
-console.log(navBar)
+const logo =document.querySelector(".logo")
+const sidebarToggle = document.querySelector(".sidebar-toggle");
 
 
 window.addEventListener("scroll", function() {
     const scrollHeight = this.window.scrollY
-    const navHeight = navBar.getBoundingClientRect().height
+    const navHeight = navBar.offsetHeight;
+    const playgroundSection = this.document.querySelector(".bg-green")
+    const playgroundSectionTop = playgroundSection.offsetTop
+
+    console.log(playgroundSectionTop)
 
     if(scrollHeight > navHeight) {
         navBar.classList.add("fixed-nav")
+    if(scrollHeight >= playgroundSectionTop) {
+        logo.classList.add("white-logo")
+        sidebarToggle.classList.add("white-sidebar-toggle")
+    } else {
+        logo.classList.remove("white-logo")
+        sidebarToggle.classList.remove("white-sidebar-toggle")
+    }
+
+
+
     } else {
         navBar.classList.remove("fixed-nav")
     }
